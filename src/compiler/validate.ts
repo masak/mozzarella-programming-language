4af2a8f29029e807a8fae2858566b41ac42fdff5
Carl Masak <carl@masak.org>
2025-10-27 22:10:48 +0800

    Vastly simpligy validator logic

--- src/compiler/validate.ts
+++ src/compiler/validate.ts
@@ -1,29 +1,9 @@
 import {
-    ArrayInitializerExpr,
     Block,
-    BlockStatement,
-    BoolLitExpr,
     CompUnit,
-    Decl,
-    DoExpr,
-    EmptyStatement,
-    Expr,
-    ExprStatement,
-    ForStatement,
-    IfStatement,
-    IndexingExpr,
-    InfixOpExpr,
-    IntLitExpr,
-    LastStatement,
-    NextStatement,
-    NoneLitExpr,
-    ParenExpr,
-    PrefixOpExpr,
-    Statement,
-    StrLitExpr,
+    SyntaxNode,
     VarDecl,
     VarRefExpr,
-    WhileStatement,
 } from "./syntax";
 
 class VarState {
@@ -33,43 +13,12 @@ class VarState {
 
 type Context = Map<string, VarState>;
 
-function validateExpr(expr: Expr, contextStack: Array<Context>): void {
-    if (expr instanceof IntLitExpr) {
-        // do nothing
-    }
-    else if (expr instanceof StrLitExpr) {
-        // do nothing
-    }
-    else if (expr instanceof BoolLitExpr) {
-        // do nothing
-    }
-    else if (expr instanceof NoneLitExpr) {
-        // do nothing
-    }
-    else if (expr instanceof PrefixOpExpr) {
-        validateExpr(expr.operand, contextStack);
-    }
-    else if (expr instanceof InfixOpExpr) {
-        validateExpr(expr.lhs, contextStack);
-        validateExpr(expr.rhs, contextStack);
-    }
-    else if (expr instanceof ParenExpr) {
-        validateExpr(expr.innerExpr, contextStack);
-    }
-    else if (expr instanceof DoExpr) {
-        validateStatement(expr.statement, contextStack);
-    }
-    else if (expr instanceof ArrayInitializerExpr) {
-        for (let element of expr.elements) {
-            validateExpr(element, contextStack);
-        }
-    }
-    else if (expr instanceof IndexingExpr) {
-        validateExpr(expr.arrayExpr, contextStack);
-        validateExpr(expr.indexExpr, contextStack);
-    }
-    else if (expr instanceof VarRefExpr) {
-        let name = expr.nameToken.payload as string;
+function visitDown(
+    syntaxNode: SyntaxNode,
+    contextStack: Array<Context>,
+): void {
+    if (syntaxNode instanceof VarRefExpr) {
+        let name = syntaxNode.nameToken.payload as string;
         for (let i = contextStack.length - 1; i >= 0; i--) {
             let context = contextStack[i];
             if (context.has(name)) {
@@ -80,75 +29,11 @@ function validateExpr(expr: Expr, contextStack: Array<Context>): void {
             }
         }
     }
-    else {
-        throw new Error(`Unknown expr type ${expr.constructor.name}`);
+    else if (syntaxNode instanceof Block) {
+        contextStack.push(new Map());
     }
-}
-
-function validateBlock(block: Block, contextStack: Array<Context>): void {
-    contextStack.push(new Map());
-    for (let statementOrDecl of block.statements) {
-        if (statementOrDecl instanceof Statement) {
-            let statement = statementOrDecl;
-            validateStatement(statement, contextStack);
-        }
-        else {  // Decl
-            let decl = statementOrDecl;
-            validateDecl(decl, contextStack);
-        }
-    }
-    contextStack.pop();
-}
-
-function validateStatement(
-    statement: Statement,
-    contextStack: Array<Context>,
-): void {
-    if (statement instanceof ExprStatement) {
-        validateExpr(statement.expr, contextStack);
-    }
-    else if (statement instanceof EmptyStatement) {
-        // do nothing
-    }
-    else if (statement instanceof BlockStatement) {
-        validateBlock(statement.block, contextStack);
-    }
-    else if (statement instanceof IfStatement) {
-        for (let clause of statement.clauseList.clauses) {
-            validateExpr(clause.condExpr, contextStack);
-            validateBlock(clause.block, contextStack);
-        }
-        if (statement.elseBlock !== null) {
-            validateBlock(statement.elseBlock, contextStack);
-        }
-    }
-    else if (statement instanceof ForStatement) {
-        validateExpr(statement.arrayExpr, contextStack);
-        validateBlock(statement.body, contextStack);
-    }
-    else if (statement instanceof WhileStatement) {
-        validateExpr(statement.condExpr, contextStack);
-        validateBlock(statement.body, contextStack);
-    }
-    else if (statement instanceof LastStatement) {
-        // do nothing
-    }
-    else if (statement instanceof NextStatement) {
-        // do nothing
-    }
-    else {
-        throw new Error(
-            `Unknown statement type ${statement.constructor.name}`
-        );
-    }
-}
-
-function validateDecl(
-    decl: Decl,
-    contextStack: Array<Context>,
-): void {
-    if (decl instanceof VarDecl) {
-        let name = decl.nameToken.payload as string;
+    else if (syntaxNode instanceof VarDecl) {
+        let name = syntaxNode.nameToken.payload as string;
         let context = contextStack[contextStack.length - 1];
         if (context.get(name) === VarState.declared) {
             throw new Error(`Redeclaration of name '${name}'`);
@@ -160,24 +45,31 @@ function validateDecl(
             context.set(name, VarState.declared);
         }
     }
-    else {
-        throw new Error(
-            `Unknown declaration type ${decl.constructor.name}`
-        );
+}
+
+function visitUp(
+    syntaxNode: SyntaxNode,
+    contextStack: Array<Context>,
+): void {
+    if (syntaxNode instanceof Block) {
+        contextStack.pop();
     }
+}
+
+function traverse(syntaxNode: SyntaxNode, contextStack: Array<Context>): void {
+    visitDown(syntaxNode, contextStack);
+
+    for (let child of syntaxNode.children) {
+        if (child instanceof SyntaxNode) {
+            traverse(child, contextStack);
+        }
+    }
+
+    visitUp(syntaxNode, contextStack);
 }
 
 export function validateCompUnit(compUnit: CompUnit): void {
     let contextStack: Array<Context> = [new Map()];
-    for (let statementOrDecl of compUnit.statements) {
-        if (statementOrDecl instanceof Statement) {
-            let statement = statementOrDecl;
-            validateStatement(statement, contextStack);
-        }
-        else {  // Decl
-            let decl = statementOrDecl;
-            validateDecl(decl, contextStack);
-        }
-    }
+    traverse(compUnit, contextStack);
 }
 
